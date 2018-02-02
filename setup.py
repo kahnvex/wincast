098dd2367ece1fcd4d580854b3b6dc5b346c8015
kahnvex <jarrod.kahn@gmail.com>
2018-02-01 23:34:54 -0800

    Remove unused deps

--- setup.py
+++ setup.py
@@ -4,22 +4,17 @@ from setuptools import setup, find_packages
 install_requires = [
     'dill==0.2.5',
     'easydict==1.6',
-    'h5py==2.6.0',
     'jsonpickle==0.9.3',
-    'Keras==1.2.0',
-    'nflgame==1.2.20',
     'numpy==1.11.2',
     'pandas==0.19.1',
     'scikit-learn==0.18.1',
     'scipy==0.18.1',
-    'tensorflow==0.12.0rc1',
-    'Theano==0.8.2',
     'tabulate==0.7.7',
 ]
 
 setup(
     name="wincast",
-    version='0.0.9',
+    version='0.0.10',
     url='https://github.com/kahnjw/wincast',
     author_email='jarrod.kahn+wincast@gmail.com',
     license='MIT',
